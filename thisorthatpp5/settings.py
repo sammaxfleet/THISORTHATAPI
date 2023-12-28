88047f16db368c16148c5bd250438cc24b7efb5a
sammaxfleet <samuelfleet@yahoo.co.uk>
2023-12-28 11:47:20 +0000

    debug = false

--- thisorthatpp5/settings.py
+++ thisorthatpp5/settings.py
@@ -70,9 +70,9 @@ SECRET_KEY = 'django-insecure-jrk1#em7)$5y*i^n6u7@hvjd!-5c=do(-x)v#2s%^q-x83c!tn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['8000-sammaxfleet-thisorthatap-vzieb9jmop.us2.codeanyapp.com',
-#                  '8000-sammaxfleet-thisorthatap-vzieb9jmop.us2.codeanyapp.com', 'thisorthatapi-56bb400a2b0e.herokuapp.com', 'localhost',]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['8000-sammaxfleet-thisorthatap-vzieb9jmop.us2.codeanyapp.com','grand-bienenstitch-624760.netlify.app','thisorthatpp5-9e3adcfaf8e9.herokuapp.com',
+                 '8000-sammaxfleet-thisorthatap-vzieb9jmop.us2.codeanyapp.com', 'thisorthatapi-56bb400a2b0e.herokuapp.com', 'localhost',]
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
