e12b879f35e85913f089b7a0faeaf6fd59a375e6
sammaxfleet <samuelfleet@yahoo.co.uk>
2024-01-02 14:52:31 +0000

    debug false

--- thisorthatpp5/settings.py
+++ thisorthatpp5/settings.py
@@ -48,12 +48,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
-# if 'DEV' not in os.environ:
-#     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-#         'rest_framework.renderers.JSONRenderer',
-#     ]
-
-
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
