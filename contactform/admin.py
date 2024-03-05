84baff99c1e12e54536c1d45399e4c3a22e5943d
sammaxfleet <samuelfleet@yahoo.co.uk>
2024-03-05 12:07:49 +0000

    added admin for contact form

--- contactform/admin.py
+++ contactform/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ContactForm
 
-# Register your models here.
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reason', 'email', 'created_at')
+    search_fields = ('user__username', 'email', 'reason')
+    list_filter = ('reason', 'created_at')
+    readonly_fields = ('created_at',)
+
+admin.site.register(ContactForm, ContactFormAdmin)
