a910772c9fdf90cf5e79947cdb9d7e2eaf105619
sammaxfleet <samuelfleet@yahoo.co.uk>
2024-03-10 11:32:48 +0000

    changed debug to true for testing

--- thisorthatpp5/settings.py
+++ thisorthatpp5/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-sammaxfleet-thisorthata-0wz3o5ksspq.ws-eu108.gitpod.io',
